533c4ae8ebcda166b1fd4494a9a09c442a9d2fb3
Rahul Prajapati <rp2628336@gmail.com>
2025-11-22 11:49:21 +0530

    Some changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-// App.js
+
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
